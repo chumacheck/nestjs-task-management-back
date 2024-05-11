ba7e4c938c9630082d61ccfd40388b4a35a91004
chumacheck <igor.olkhin@gmail.com>
2024-05-11 15:53:30 +0500

    Project

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,65 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { User } from 'src/auth/user.entity';
+import { error } from 'console';
+
+@Injectable()
+export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository', { timestamp: true });
+
+  constructor(private dataSource: DataSource) {
+    super(Task, dataSource.createEntityManager());
+  }
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+      user,
+    });
+
+    await this.save(task);
+    return task;
+  }
+
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+    const query = this.createQueryBuilder('task');
+
+    const { status, search } = filterDto;
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        '(LOWER(task.descriprion) LIKE LOWER(:search) OR LOWER(task.title) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
+    }
+
+    query.andWhere({ user });
+
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user ${user.username}. Filters: ${JSON.stringify(filterDto)}.`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
+}
